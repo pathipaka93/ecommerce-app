bda1358e204a580822807340541a3f9535879fb8
Sujita Rani Kadarla <kadarlas@Sujitas-MacBook-Pro.local>
2022-09-04 17:40:42 -0400

    signInWithGoogleRedirect

--- src/routes/signIn/signIn.component.jsx
+++ src/routes/signIn/signIn.component.jsx
@@ -1,6 +1,25 @@
-import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../Utils/firebase/firebase.utils.js'
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
+import {
+    auth,
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect
+} from '../../Utils/firebase/firebase.utils.js'
 
 const SignIn = () => {
+    useEffect( () => {
+        const xyz = async () => { 
+            const response = await getRedirectResult(auth);
+            console.log(response);
+            if(response){
+                const userDocRef = createUserDocumentFromAuth(response.user)
+            }
+        }
+        xyz();
+    },[]);
+
     const logGoogleUser =  async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = createUserDocumentFromAuth(user);
@@ -10,6 +29,10 @@ const SignIn = () => {
         <div>
             <h2>Sign In Page</h2>
             <button onClick = {logGoogleUser}>Sign In with Google Pop up</button>
+            <br/>
+            <br/>
+            <button onClick = {signInWithGoogleRedirect}>Sign In with Google Redirect</button>
+
         </div>
     )
 }
